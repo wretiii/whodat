32811678fc57ae1a5243d15bd2502ffb6afd442b
wretiii <28065608+wretiii@users.noreply.github.com>
2023-08-07 13:21:22 -0400

    Create whodat.sh

---
+++ whodat.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Check if a file name is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <file>"
+    exit 1
+fi
+
+# Check if the provided file exists
+if [ ! -f "$1" ]; then
+    echo "File not found: $1"
+    exit 1
+fi
+
+# Read each line from the file
+while read -r line; do
+    echo "Processing: $line"
+
+    # Perform whois command and extract "Registrant Name:"
+    registrant_name=$(whois "$line" | awk -F ':' '/Registrant Name:/ {print $2; exit}')
+
+    if [ -n "$registrant_name" ]; then
+        echo "Registrant Name: $registrant_name"
+    else
+        echo "Registrant Name not found"
+    fi
+
+    # Perform fping command and extract IP address
+    fping_output=$(fping -A -n "$line" 2>&1)
+    
+    if [ $? -eq 0 ]; then
+        ip_address=$(echo "$fping_output" | awk '{print $2}')
+        echo "fping: Host is reachable, IP: $ip_address"
+    else
+        echo "fping: Host is unreachable"
+    fi
+
+    echo
+done < "$1"
